ceea52b103f63185bac5ed40bf9ef0d26969e2e8
Bao Hengtao <baohengtao@users.noreply.github.com>
2026-04-27 15:53:17 +0800

    🧩 refactor(database): use BaseModel from toolkit

--- redbook/model.py
+++ redbook/model.py
@@ -1,17 +1,16 @@
 import re
-from datetime import datetime
 from pathlib import Path
 from typing import AsyncGenerator, Iterator, Self
 
 import keyring
 import pendulum
-from peewee import Model
 from photosinfo.model import GirlSearch
 from playhouse.postgres_ext import PostgresqlExtDatabase
 from playhouse.shortcuts import model_to_dict, update_model_from_dict
 from rich.prompt import Confirm
 from toolkit.model import (
-    ArrayField, BooleanField,
+    ArrayField, BaseModel,
+    BooleanField,
     DateTimeTZField,
     ForeignKeyField,
     IntegerField, JSONField,
@@ -37,32 +36,7 @@ database = PostgresqlExtDatabase(
     sslmode="disable",
     gssencmode="disable",
     connect_timeout=3)
-
-
-class BaseModel(Model):
-    class Meta:
-        database = database
-
-    def __repr__(self):
-        model = model_to_dict(self, recurse=False)
-        for k, v in model.items():
-            if isinstance(v, datetime):
-                model[k] = v.strftime("%Y-%m-%d %H:%M:%S")
-
-        return "\n".join(f'{k}: {v}'.replace('\n', '  ') for k, v
-                         in model.items() if v is not None)
-
-    @classmethod
-    def get_or_none(cls, *query, **filters) -> Self | None:
-        return super().get_or_none(*query, **filters)
-
-    @classmethod
-    def get(cls, *query, **filters) -> Self:
-        return super().get(*query, **filters)
-
-    @classmethod
-    def get_by_id(cls, *query, **filters) -> Self:
-        return super().get_by_id(*query, **filters)
+BaseModel.bind(database)
 
 
 class User(BaseModel):
